50e9f1659480509b27dd4088f2a3b83ca82f250c
GotyLP <lidiamportiz@gmail.com>
2025-06-15 14:32:27 -0300

    wip shaders

---
+++ Assets/Scripts/Editor/RoadGeneratorEditor.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RoadGenerator))]
+public class RoadGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        
+        DrawDefaultInspector();
+
+        RoadGenerator roadGenerator = (RoadGenerator)target;
+        
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Generate Road"))
+        {
+            roadGenerator.GenerateRoadMesh();
+            SceneView.RepaintAll();
+        }
+        
+        serializedObject.ApplyModifiedProperties();
+    }
+}
